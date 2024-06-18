bdcf8b60678b0b0c2f027c710062a493f1809073
dewiaanjn <dewianjaniwibowo@gmail.com>
2024-06-18 08:39:28 +0700

    update-frontbackend

--- FrontEnd/js/script.js
+++
@@ -1,8 +0,0 @@
-function showMoreInfo() {
-    var moreInfoDiv = document.getElementById('more-info');
-    if (moreInfoDiv.classList.contains('hidden')) {
-        moreInfoDiv.classList.remove('hidden');
-    } else {
-        moreInfoDiv.classList.add('hidden');
-    }
-}
